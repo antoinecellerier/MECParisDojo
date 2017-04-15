d7bb02eb9aec54d5b733f0561102c28a8a2c8ab0
Antoine Cellerier <antoine.cellerier@microsoft.com>
2017-04-15 18:08:57 +0200

    Stop using classes

--- 2017-04-13/bastte/DiceScorer/DiceScorer.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorer.cs
@@ -14,104 +14,82 @@ namespace DiceScorer
         public static readonly int[] ZeroDice = {0, 0, 0, 0, 0, 0};
     }
 
-    interface ScoreRule
-    {
-        (int score, int[] remaining) Score(int[] counts);
-    }
-
-    public class SingleDieScoreRule : ScoreRule
-    {
-        private readonly int _value;
-        private readonly int _score;
-
-        public SingleDieScoreRule(int value, int score)
-        {
-            _value = value;
-            _score = score;
-        }
-
-        public (int score, int[] remaining) Score(int[] counts)
-        {
-            int score = 0;
-
-            if (counts[_value-1] == 1)
-            {
-                score += _score;
-                counts = counts.ToArray();
-                counts[_value-1] = 0;
-            }
-
-            return (score, counts);
-        }
-    }
-
-    class TripleAndMoreScoreDice : ScoreRule
-    {
-        public (int score, int[] remaining) Score(int[] counts)
-        {
-            bool cloned = false;
-            return (Enumerable.Range(0, 6)
-                .Aggregate(0, (score, value) =>
-                {
-                    if (counts[value] >= 3)
-                    {
-                        score += (value == 0 ? 1000 : 100 * (value + 1)) << (counts[value] - 3);
-                        if (!cloned)
-                        {
-                            cloned = true;
-                            counts = counts.ToArray();
-                        }
-                        counts[value] = 0;
-                    }
-                    return score;
-                }), counts);
-        }
-    }
-
-    public class PairScoreRule : ScoreRule
-    {
-        public (int score, int[] remaining) Score(int[] counts)
-        {
-            if (counts.Sum() == 6 && counts.All(count => count == 0 || count == 2))
-            {
-                return (800, ZeroDice);
-            }
-            return (0, counts);
-        }
-    }
-
-    public class StraightScoreRule : ScoreRule
-    {
-        public (int score, int[] remaining) Score(int[] counts)
-        {
-            if (counts.All(count => count == 1))
-            {
-                return (1200, ZeroDice);
-            }
-            return (0, counts);
-        }
-    }
 
     public class DiceScorer
     {
-        private readonly List<ScoreRule> _rules = new List<ScoreRule>
-        {
-            new PairScoreRule(),
-            new StraightScoreRule(),
-            new TripleAndMoreScoreDice(),
-            new SingleDieScoreRule(1, 100),
-            new SingleDieScoreRule(5, 50)
-        };
-
         public int Score(List<int> dice)
         {
             int[] originalCounts = DiceValues.Select(value => dice.Count(die => die == value)).ToArray();
 
-            return _rules.Aggregate<ScoreRule, (int score, int[] counts)>(
-                    (0, originalCounts),
-                    ((int score, int[] counts) _, ScoreRule rule) =>
+            return new List<Func<int[], (int score, int[] counts)>>
+            {
+                counts =>
+                {
+                    if (counts.Sum() == 6 && counts.All(count => count == 0 || count == 2))
                     {
-                        (int score, int[] counts) = rule.Score(_.counts);
+                        return (800, ZeroDice);
+                    }
+                    return (0, counts);
+                },
+                counts =>
+                {
+                    if (counts.All(count => count == 1))
+                    {
+                        return (1200, ZeroDice);
+                    }
+                    return (0, counts);
+                },
+                counts =>
+                {
+                    bool cloned = false;
+                    return (Enumerable.Range(0, 6)
+                        .Aggregate(0, (score, value) =>
+                        {
+                            if (counts[value] >= 3)
+                            {
+                                score += (value == 0 ? 1000 : 100 * (value + 1)) << (counts[value] - 3);
+                                if (!cloned)
+                                {
+                                    cloned = true;
+                                    counts = counts.ToArray();
+                                }
+                                counts[value] = 0;
+                            }
+                            return score;
+                        }), counts);
+                },
+                counts =>
+                {
+                    int score = 0;
+
+                    if (counts[1-1] == 1)
+                    {
+                        score += 100;
+                        counts = counts.ToArray();
+                        counts[1-1] = 0;
+                    }
+
+                    return (score, counts);
+                },
+                counts =>
+                {
+                    int score = 0;
+
+                    if (counts[5-1] == 1)
+                    {
+                        score += 50;
+                        counts = counts.ToArray();
+                        counts[5-1] = 0;
+                    }
+
+                    return (score, counts);
+                }
+            }
+                .Aggregate<Func<int[], (int score, int[] counts)>, (int score, int[] counts)>(
+                    (0, originalCounts),
+                    ((int score, int[] counts) _, Func<int[], (int score, int[] counts)> rule) =>
+                    {
+                        (int score, int[] counts) = rule(_.counts);
                         return (_.score + score, counts);
                     })
                 .score;
