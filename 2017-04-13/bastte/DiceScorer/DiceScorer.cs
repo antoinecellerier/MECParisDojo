f0e2a422238d20db96272ceb195a9fcb3c235714
Antoine Cellerier <antoine.cellerier@microsoft.com>
2017-04-15 18:27:01 +0200

    Make rule writting more concise

--- 2017-04-13/bastte/DiceScorer/DiceScorer.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorer.cs
@@ -1,87 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DiceScorer
 {
-    using static ScoreConstants;
-
-    public static class ScoreConstants
-    {
-        public static readonly int[] DiceValues = Enumerable.Range(1, 6).ToArray();
-        public static readonly int[] ZeroDice = {0, 0, 0, 0, 0, 0};
-    }
-
-
     public class DiceScorer
     {
+        private static readonly int[] DiceValues = Enumerable.Range(1, 6).ToArray();
+        private static readonly int[] ZeroDice = { 0, 0, 0, 0, 0, 0 };
+
+        private static Func<int[], (int score, int[] counts)> Rule(Predicate<int[]> condition, int score,
+            Func<int[], int[]> cost)
+        {
+            return Rule(condition, _ => score, cost);
+        }
+
+        private static Func<int[], (int score, int[] counts)> Rule(Predicate<int[]> condition, Func<int[], int> score,
+            Func<int[], int[]> cost)
+        {
+            return counts => !condition(counts) ? (0, counts) : (score(counts), cost(counts.ToArray()));
+        }
+
         private readonly IEnumerable<Func<int[], (int score, int[] counts)>> _rules =
             Enumerable.Range(0, 6)
-            .Select<int, Func<int[], (int score, int[] counts)>>(value => ((int[] counts) =>
-                {
-                    int c = counts[value] - 3;
-                    if (c < 0)
-                    {
-                        return (0, counts);
-                    }
-
-                    counts = counts.ToArray();
-                    counts[value] = 0;
-                    return ((value == 0 ? 1000 : 100 * (value + 1)) << c,
-                        counts);
-                }))
-            .Concat(
-                new List<Func<int[], (int score, int[] counts)>>
-                {
-                    counts =>
-                    {
-                        if (counts.Sum() == 6 && counts.All(count => count == 0 || count == 2))
+                .Select<int, Func<int[], (int score, int[] counts)>>(value =>
+                    Rule(counts => counts[value] >= 3,
+                        counts => (value == 0 ? 1000 : 100 * (value + 1)) << (counts[value] - 3), counts =>
                         {
-                            return (800, ZeroDice);
-                        }
-                        return (0, counts);
-                    },
-                    counts =>
+                            counts[value] = 0;
+                            return counts;
+                        }))
+                .Concat(
+                    new List<Func<int[], (int score, int[] counts)>>
                     {
-                        if (counts.All(count => count == 1))
+                        Rule(counts => counts.Sum() == 6 && counts.All(count => count == 0 || count == 2), 800,
+                            counts => ZeroDice),
+                        Rule(counts => counts.All(count => count == 1), 1200, counts => ZeroDice),
+                        Rule(counts => counts[1 - 1] == 1, 100, counts =>
                         {
-                            return (1200, ZeroDice);
-                        }
-                        return (0, counts);
-                    },
-                    counts =>
-                    {
-                        if (counts[1 - 1] != 1)
+                            counts[1 - 1] = 0;
+                            return counts;
+                        }),
+                        Rule(counts => counts[5 - 1] == 1, 50, counts =>
                         {
-                            return (0, counts);
-                        }
-
-                        counts = counts.ToArray();
-                        counts[1 - 1] = 0;
-
-                        return (100, counts);
-                    },
-                    counts =>
-                    {
-                        if (counts[5 - 1] != 1)
-                        {
-                            return (0, counts);
-                        }
-
-                        counts = counts.ToArray();
-                        counts[5 - 1] = 0;
-
-                        return (50, counts);
-                    }
-                });
+                            counts[5 - 1] = 0;
+                            return counts;
+                        })
+                    });
 
         public int Score(List<int> dice)
         {
             int[] originalCounts = DiceValues.Select(value => dice.Count(die => die == value)).ToArray();
-
-
 
             return _rules.Aggregate<Func<int[], (int score, int[] counts)>, (int score, int[] counts)>(
                     (0, originalCounts),
