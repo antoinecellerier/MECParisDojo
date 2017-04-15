56bdb3ce2bb4012c50edcdd1728274b8fb33384b
Antoine Cellerier <antoine.cellerier@microsoft.com>
2017-04-15 18:41:55 +0200

    More consice code (no structural changes)

--- 2017-04-13/bastte/DiceScorer/DiceScorer.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorer.cs
@@ -9,16 +9,16 @@ namespace DiceScorer
         private static readonly int[] DiceValues = Enumerable.Range(1, 6).ToArray();
         private static readonly int[] ZeroDice = { 0, 0, 0, 0, 0, 0 };
 
-        private static Func<int[], (int score, int[] counts)> Rule(Predicate<int[]> condition, int score,
+        private static Func<int[], (int s, int[] c)> Rule(Predicate<int[]> condition, int s,
             Func<int[], int[]> cost)
         {
-            return Rule(condition, _ => score, cost);
+            return Rule(condition, _ => s, cost);
         }
 
-        private static Func<int[], (int score, int[] counts)> Rule(Predicate<int[]> condition, Func<int[], int> score,
+        private static Func<int[], (int s, int[] c)> Rule(Predicate<int[]> condition, Func<int[], int> s,
             Func<int[], int[]> cost)
         {
-            return counts => !condition(counts) ? (0, counts) : (score(counts), cost(counts));
+            return c => !condition(c) ? (0, c) : (s(c), cost(c));
         }
 
         private static readonly Func<int[], int[]> Zero = _ => ZeroDice;
@@ -32,33 +32,33 @@ namespace DiceScorer
             };
         }
 
-        private readonly IEnumerable<Func<int[], (int score, int[] counts)>> _rules =
+        private readonly IEnumerable<Func<int[], (int s, int[] c)>> _rules =
             Enumerable.Range(0, 6)
-                .Select<int, Func<int[], (int score, int[] counts)>>(value =>
-                    Rule(counts => counts[value] >= 3,
-                        counts => (value == 0 ? 1000 : 100 * (value + 1)) << (counts[value] - 3),
+                .Select(value =>
+                    Rule(c => c[value] >= 3,
+                        c => (value == 0 ? 1000 : 100 * (value + 1)) << (c[value] - 3),
                         CloneZero(value)))
                 .Concat(
-                    new List<Func<int[], (int score, int[] counts)>>
+                    new[]
                     {
-                        Rule(counts => counts.Sum() == 6 && counts.All(count => count == 0 || count == 2), 800, Zero),
-                        Rule(counts => counts.All(count => count == 1), 1200, Zero),
-                        Rule(counts => counts[0] == 1, 100, CloneZero(0)),
-                        Rule(counts => counts[4] == 1, 50, CloneZero(4))
+                        Rule(c => c.Sum() == 6 && c.All(_ => _ == 0 || _ == 2), 800, Zero),
+                        Rule(c => c.All(count => count == 1), 1200, Zero),
+                        Rule(c => c[0] == 1, 100, CloneZero(0)),
+                        Rule(c => c[4] == 1, 50, CloneZero(4))
                     });
 
         public int Score(List<int> dice)
         {
-            int[] originalCounts = DiceValues.Select(value => dice.Count(die => die == value)).ToArray();
+            int[] originalc = DiceValues.Select(value => dice.Count(die => die == value)).ToArray();
 
-            return _rules.Aggregate<Func<int[], (int score, int[] counts)>, (int score, int[] counts)>(
-                    (0, originalCounts),
-                    ((int score, int[] counts) _, Func<int[], (int score, int[] counts)> rule) =>
+            return _rules.Aggregate<Func<int[], (int s, int[] c)>, (int s, int[] c)>(
+                    (0, originalc),
+                    (_, rule) =>
                     {
-                        (int score, int[] counts) = rule(_.counts);
-                        return (_.score + score, counts);
+                        (int s, int[] c) = rule(_.c);
+                        return (_.s + s, c);
                     })
-                .score;
+                .s;
         }
     }
 }
