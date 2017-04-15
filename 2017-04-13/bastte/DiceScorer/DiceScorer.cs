c89b26aa794ea1ac99c026d8fa1abcb9ed59e497
Antoine Cellerier <antoine.cellerier@microsoft.com>
2017-04-15 18:47:35 +0200

    Remove useless brackets

--- 2017-04-13/bastte/DiceScorer/DiceScorer.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorer.cs
@@ -7,30 +7,23 @@ namespace DiceScorer
     public class DiceScorer
     {
         private static readonly int[] DiceValues = Enumerable.Range(1, 6).ToArray();
-        private static readonly int[] ZeroDice = { 0, 0, 0, 0, 0, 0 };
+        private static readonly int[] ZeroDice = {0, 0, 0, 0, 0, 0};
 
         private static Func<int[], (int s, int[] c)> Rule(Predicate<int[]> condition, int s,
-            Func<int[], int[]> cost)
-        {
-            return Rule(condition, _ => s, cost);
-        }
+            Func<int[], int[]> cost) => Rule(condition, _ => s, cost);
 
         private static Func<int[], (int s, int[] c)> Rule(Predicate<int[]> condition, Func<int[], int> s,
-            Func<int[], int[]> cost)
-        {
-            return c => !condition(c) ? (0, c) : (s(c), cost(c));
-        }
+            Func<int[], int[]> cost) => c => !condition(c) ? (0, c) : (s(c), cost(c));
 
         private static readonly Func<int[], int[]> Zero = _ => ZeroDice;
-        private static Func<int[], int[]> CloneZero(int index)
-        {
-            return src =>
+
+        private static Func<int[], int[]> CloneZero(int index) =>
+            src =>
             {
                 int[] dst = src.ToArray();
                 dst[index] = 0;
                 return dst;
             };
-        }
 
         private readonly IEnumerable<Func<int[], (int s, int[] c)>> _rules =
             Enumerable.Range(0, 6)
@@ -47,18 +40,14 @@ namespace DiceScorer
                         Rule(c => c[4] == 1, 50, CloneZero(4))
                     });
 
-        public int Score(List<int> dice)
-        {
-            int[] originalc = DiceValues.Select(value => dice.Count(die => die == value)).ToArray();
-
-            return _rules.Aggregate<Func<int[], (int s, int[] c)>, (int s, int[] c)>(
-                    (0, originalc),
+        public int Score(List<int> dice) =>
+            _rules.Aggregate<Func<int[], (int s, int[] c)>, (int s, int[] c)>(
+                    (0, DiceValues.Select(value => dice.Count(die => die == value)).ToArray()),
                     (_, rule) =>
                     {
                         (int s, int[] c) = rule(_.c);
                         return (_.s + s, c);
                     })
                 .s;
-        }
     }
 }
