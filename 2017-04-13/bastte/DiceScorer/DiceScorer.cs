a7f2d3b4ec791c1d312a3702b2471754e413fab5
Antoine Cellerier <antoine.cellerier@microsoft.com>
2017-04-15 22:50:34 +0200

    Switch to using ImmutableArray

--- 2017-04-13/bastte/DiceScorer/DiceScorer.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorer.cs
@@ -1,31 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using Counts = System.Collections.Immutable.ImmutableArray<int>;
+using Score = System.Int32;
 
 namespace DiceScorer
 {
     public class DiceScorer
     {
         private static readonly int[] DiceValues = Enumerable.Range(1, 6).ToArray();
-        private static readonly int[] ZeroDice = {0, 0, 0, 0, 0, 0};
+        private static readonly Counts ZeroDice = new[]{0, 0, 0, 0, 0, 0}.ToImmutableArray();
 
-        private static Func<int[], (int s, int[] c)> Rule(Predicate<int[]> condition, int s,
-            Func<int[], int[]> cost) => Rule(condition, _ => s, cost);
+        private static Func<Counts, (Score s, Counts c)> Rule(Predicate<Counts> condition, Score s,
+            Func<Counts, Counts> cost) => Rule(condition, _ => s, cost);
 
-        private static Func<int[], (int s, int[] c)> Rule(Predicate<int[]> condition, Func<int[], int> s,
-            Func<int[], int[]> cost) => c => !condition(c) ? (0, c) : (s(c), cost(c));
+        private static Func<Counts, (Score s, Counts c)> Rule(Predicate<Counts> condition, Func<Counts, Score> s,
+            Func<Counts, Counts> cost) => c => !condition(c) ? (0, c) : (s(c), cost(c));
 
-        private static readonly Func<int[], int[]> Zero = _ => ZeroDice;
+        private static readonly Func<Counts, Counts> Zero = _ => ZeroDice;
 
-        private static Func<int[], int[]> CloneZero(int index) =>
-            src =>
-            {
-                int[] dst = src.ToArray();
-                dst[index] = 0;
-                return dst;
-            };
+        private static Func<Counts, Counts> CloneZero(int index) => src => src.SetItem(index, 0);
 
-        private readonly IEnumerable<Func<int[], (int s, int[] c)>> _rules =
+        private readonly IEnumerable<Func<Counts, (Score s, Counts c)>> _rules =
             Enumerable.Range(0, 6)
                 .Select(value =>
                     Rule(c => c[value] >= 3,
@@ -41,11 +38,11 @@ namespace DiceScorer
                     });
 
         public int Score(List<int> dice) =>
-            _rules.Aggregate<Func<int[], (int s, int[] c)>, (int s, int[] c)>(
-                    (0, DiceValues.Select(value => dice.Count(die => die == value)).ToArray()),
+            _rules.Aggregate<Func<Counts, (Score s, Counts c)>, (Score s, Counts c)>(
+                    (0, DiceValues.Select(value => dice.Count(die => die == value)).ToImmutableArray()),
                     (_, rule) =>
                     {
-                        (int s, int[] c) = rule(_.c);
+                        (Score s, Counts c) = rule(_.c);
                         return (_.s + s, c);
                     })
                 .s;
