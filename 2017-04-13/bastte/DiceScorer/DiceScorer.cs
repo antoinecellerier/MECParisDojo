c8b5a2daca9edc69a46336610323033758d8d923
Antoine Cellerier <antoine.cellerier@microsoft.com>
2017-04-15 18:36:38 +0200

    More concise cost transforms

--- 2017-04-13/bastte/DiceScorer/DiceScorer.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorer.cs
@@ -18,34 +18,33 @@ namespace DiceScorer
         private static Func<int[], (int score, int[] counts)> Rule(Predicate<int[]> condition, Func<int[], int> score,
             Func<int[], int[]> cost)
         {
-            return counts => !condition(counts) ? (0, counts) : (score(counts), cost(counts.ToArray()));
+            return counts => !condition(counts) ? (0, counts) : (score(counts), cost(counts));
+        }
+
+        private static readonly Func<int[], int[]> Zero = _ => ZeroDice;
+        private static Func<int[], int[]> CloneZero(int index)
+        {
+            return src =>
+            {
+                int[] dst = src.ToArray();
+                dst[index] = 0;
+                return dst;
+            };
         }
 
         private readonly IEnumerable<Func<int[], (int score, int[] counts)>> _rules =
             Enumerable.Range(0, 6)
                 .Select<int, Func<int[], (int score, int[] counts)>>(value =>
                     Rule(counts => counts[value] >= 3,
-                        counts => (value == 0 ? 1000 : 100 * (value + 1)) << (counts[value] - 3), counts =>
-                        {
-                            counts[value] = 0;
-                            return counts;
-                        }))
+                        counts => (value == 0 ? 1000 : 100 * (value + 1)) << (counts[value] - 3),
+                        CloneZero(value)))
                 .Concat(
                     new List<Func<int[], (int score, int[] counts)>>
                     {
-                        Rule(counts => counts.Sum() == 6 && counts.All(count => count == 0 || count == 2), 800,
-                            counts => ZeroDice),
-                        Rule(counts => counts.All(count => count == 1), 1200, counts => ZeroDice),
-                        Rule(counts => counts[1 - 1] == 1, 100, counts =>
-                        {
-                            counts[1 - 1] = 0;
-                            return counts;
-                        }),
-                        Rule(counts => counts[5 - 1] == 1, 50, counts =>
-                        {
-                            counts[5 - 1] = 0;
-                            return counts;
-                        })
+                        Rule(counts => counts.Sum() == 6 && counts.All(count => count == 0 || count == 2), 800, Zero),
+                        Rule(counts => counts.All(count => count == 1), 1200, Zero),
+                        Rule(counts => counts[0] == 1, 100, CloneZero(0)),
+                        Rule(counts => counts[4] == 1, 50, CloneZero(4))
                     });
 
         public int Score(List<int> dice)
