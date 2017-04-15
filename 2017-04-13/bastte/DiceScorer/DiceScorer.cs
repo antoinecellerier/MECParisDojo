e62c3ac603a885f6f9611c25f97e98065e436dea
Antoine Cellerier <antoine.cellerier@microsoft.com>
2017-04-15 23:10:48 +0200

    Aggregate rules as function

--- 2017-04-13/bastte/DiceScorer/DiceScorer.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorer.cs
@@ -10,19 +10,21 @@ namespace DiceScorer
     public class DiceScorer
     {
         private static readonly int[] DiceValues = Enumerable.Range(1, 6).ToArray();
-        private static readonly Counts ZeroDice = new[]{0, 0, 0, 0, 0, 0}.ToImmutableArray();
+        private static readonly Counts ZeroDice = Enumerable.Repeat(0, 6).ToImmutableArray();
 
-        private static Func<Counts, (Score s, Counts c)> Rule(Predicate<Counts> condition, Score s,
-            Func<Counts, Counts> cost) => Rule(condition, _ => s, cost);
+        private static Func<(Score, Counts), (Score s, Counts c)> Rule(
+            Predicate<Counts> condition, Score score, Func<Counts, Counts> cost)
+            => Rule(condition, _ => score, cost);
 
-        private static Func<Counts, (Score s, Counts c)> Rule(Predicate<Counts> condition, Func<Counts, Score> s,
-            Func<Counts, Counts> cost) => c => !condition(c) ? (0, c) : (s(c), cost(c));
+        private static Func<(Score, Counts), (Score s, Counts c)> Rule(
+            Predicate<Counts> condition, Func<Counts, Score> score, Func<Counts, Counts> cost)
+            => ((Score s, Counts c) _) => !condition(_.c) ? _ : (_.s + score(_.c), cost(_.c));
 
         private static readonly Func<Counts, Counts> Zero = _ => ZeroDice;
 
         private static Func<Counts, Counts> CloneZero(int index) => src => src.SetItem(index, 0);
 
-        private readonly IEnumerable<Func<Counts, (Score s, Counts c)>> _rules =
+        private readonly Func<(Score, Counts), (Score s, Counts c)> _rules =
             Enumerable.Range(0, 6)
                 .Select(value =>
                     Rule(c => c[value] >= 3,
@@ -32,19 +34,14 @@ namespace DiceScorer
                     new[]
                     {
                         Rule(c => c.Sum() == 6 && c.All(_ => _ == 0 || _ == 2), 800, Zero),
-                        Rule(c => c.All(count => count == 1), 1200, Zero),
+                        Rule(c => c.All(_ => _ == 1), 1200, Zero),
                         Rule(c => c[0] == 1, 100, CloneZero(0)),
                         Rule(c => c[4] == 1, 50, CloneZero(4))
-                    });
-
-        public int Score(List<int> dice) =>
-            _rules.Aggregate<Func<Counts, (Score s, Counts c)>, (Score s, Counts c)>(
-                    (0, DiceValues.Select(value => dice.Count(die => die == value)).ToImmutableArray()),
-                    (_, rule) =>
-                    {
-                        (Score s, Counts c) = rule(_.c);
-                        return (_.s + s, c);
                     })
-                .s;
+                .Aggregate((f, g) => _ => g(f(_)));
+
+        public int Score(List<int> dice) => _rules((0,
+                DiceValues.Select(v => dice.Count(d => d == v)).ToImmutableArray()))
+            .s;
     }
 }
