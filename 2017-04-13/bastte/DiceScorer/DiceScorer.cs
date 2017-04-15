08cbd883479d1260e6ee1c425eb3ee314c255f86
Antoine Cellerier <antoine.cellerier@microsoft.com>
2017-04-15 18:18:36 +0200

    Flatten rule definition

--- 2017-04-13/bastte/DiceScorer/DiceScorer.cs
+++ 2017-04-13/bastte/DiceScorer/DiceScorer.cs
@@ -17,75 +17,73 @@ namespace DiceScorer
 
     public class DiceScorer
     {
+        private readonly IEnumerable<Func<int[], (int score, int[] counts)>> _rules =
+            Enumerable.Range(0, 6)
+            .Select<int, Func<int[], (int score, int[] counts)>>(value => ((int[] counts) =>
+                {
+                    int c = counts[value] - 3;
+                    if (c < 0)
+                    {
+                        return (0, counts);
+                    }
+
+                    counts = counts.ToArray();
+                    counts[value] = 0;
+                    return ((value == 0 ? 1000 : 100 * (value + 1)) << c,
+                        counts);
+                }))
+            .Concat(
+                new List<Func<int[], (int score, int[] counts)>>
+                {
+                    counts =>
+                    {
+                        if (counts.Sum() == 6 && counts.All(count => count == 0 || count == 2))
+                        {
+                            return (800, ZeroDice);
+                        }
+                        return (0, counts);
+                    },
+                    counts =>
+                    {
+                        if (counts.All(count => count == 1))
+                        {
+                            return (1200, ZeroDice);
+                        }
+                        return (0, counts);
+                    },
+                    counts =>
+                    {
+                        if (counts[1 - 1] != 1)
+                        {
+                            return (0, counts);
+                        }
+
+                        counts = counts.ToArray();
+                        counts[1 - 1] = 0;
+
+                        return (100, counts);
+                    },
+                    counts =>
+                    {
+                        if (counts[5 - 1] != 1)
+                        {
+                            return (0, counts);
+                        }
+
+                        counts = counts.ToArray();
+                        counts[5 - 1] = 0;
+
+                        return (50, counts);
+                    }
+                });
+
         public int Score(List<int> dice)
         {
             int[] originalCounts = DiceValues.Select(value => dice.Count(die => die == value)).ToArray();
 
-            return new List<Func<int[], (int score, int[] counts)>>
-            {
-                counts =>
-                {
-                    if (counts.Sum() == 6 && counts.All(count => count == 0 || count == 2))
-                    {
-                        return (800, ZeroDice);
-                    }
-                    return (0, counts);
-                },
-                counts =>
-                {
-                    if (counts.All(count => count == 1))
-                    {
-                        return (1200, ZeroDice);
-                    }
-                    return (0, counts);
-                },
-                counts =>
-                {
-                    bool cloned = false;
-                    return (Enumerable.Range(0, 6)
-                        .Aggregate(0, (score, value) =>
-                        {
-                            if (counts[value] >= 3)
-                            {
-                                score += (value == 0 ? 1000 : 100 * (value + 1)) << (counts[value] - 3);
-                                if (!cloned)
-                                {
-                                    cloned = true;
-                                    counts = counts.ToArray();
-                                }
-                                counts[value] = 0;
-                            }
-                            return score;
-                        }), counts);
-                },
-                counts =>
-                {
-                    int score = 0;
 
-                    if (counts[1-1] == 1)
-                    {
-                        score += 100;
-                        counts = counts.ToArray();
-                        counts[1-1] = 0;
-                    }
 
-                    return (score, counts);
-                },
-                counts =>
-                {
-                    int score = 0;
-
-                    if (counts[5-1] == 1)
-                    {
-                        score += 50;
-                        counts = counts.ToArray();
-                        counts[5-1] = 0;
-                    }
-
-                    return (score, counts);
-                }
-            }
-                .Aggregate<Func<int[], (int score, int[] counts)>, (int score, int[] counts)>(
+            return _rules.Aggregate<Func<int[], (int score, int[] counts)>, (int score, int[] counts)>(
                     (0, originalCounts),
                     ((int score, int[] counts) _, Func<int[], (int score, int[] counts)> rule) =>
                     {
