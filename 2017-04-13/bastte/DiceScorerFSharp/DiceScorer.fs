35798b279353d24e0256cbde0dd4b22359e713d4
Antoine Cellerier <antoine.cellerier@microsoft.com>
2017-04-17 17:22:03 +0200

    Fix spacing

--- 2017-04-13/bastte/DiceScorerFSharp/DiceScorer.fs
+++ 2017-04-13/bastte/DiceScorerFSharp/DiceScorer.fs
@@ -5,10 +5,10 @@ type DiceScorer() =
         match c with
             | (1, c) :: xs when c >= 3 -> (1000<<<(c-3)) + Rules xs
             | (v, c) :: xs when c >= 3 -> v * (100<<<(c-3)) + Rules xs
-            | (_,2) :: (_,2) :: (_,2) :: xs -> 800 + Rules xs
-            | (_,1) :: (_,1) :: (_,1) :: (_,1) :: (_,1) :: (_,1) :: xs -> 1200 + Rules xs
-            | (1,1) :: xs -> 100 + Rules xs
-            | (5,1) :: xs -> 50 + Rules xs
+            | (_, 2) :: (_, 2) :: (_, 2) :: xs -> 800 + Rules xs
+            | (_, 1) :: (_, 1) :: (_, 1) :: (_, 1) :: (_, 1) :: (_, 1) :: xs -> 1200 + Rules xs
+            | (1, 1) :: xs -> 100 + Rules xs
+            | (5, 1) :: xs -> 50 + Rules xs
             | x :: xs -> Rules xs
             | [] -> 0
 
